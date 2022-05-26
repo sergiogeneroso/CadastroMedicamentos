4550ddd1ef6552346d56df5d10f124b225f192fe
sergio.generoso <sergio.generoso@eagletecnologia.com.br>
2022-05-26 20:31:29 -0300

    Alteracoes arquivo dpr e dproj

--- packages/avaliacao/ProjetoDeAvaliacao.dpr
+++ packages/avaliacao/ProjetoDeAvaliacao.dpr
@@ -4,7 +4,11 @@ uses
   Vcl.Forms,
   Avaliacao in '..\..\src\Avaliacao.pas' {frmPrincipal},
   Avaliacao.Conexao in '..\..\src\conexao\Avaliacao.Conexao.pas',
-  Avaliacao.Impl.Conexao in '..\..\src\conexao\impl\Avaliacao.Impl.Conexao.pas';
+  Avaliacao.Impl.Conexao in '..\..\src\conexao\impl\Avaliacao.Impl.Conexao.pas',
+  Avalicacao.Cadastros.Fabricante.Model.Entity.Fabricante in '..\..\src\cadastros\fabricante\model\entity\Avalicacao.Cadastros.Fabricante.Model.Entity.Fabricante.pas',
+  Avalicacao.Cadastros.Fabricante.Model.Entity.Impl.Fabricante in '..\..\src\cadastros\fabricante\model\entity\impl\Avalicacao.Cadastros.Fabricante.Model.Entity.Impl.Fabricante.pas',
+  Avalicacao.Cadastros.Fabricante.Model.Entity.Impl.FabricanteRepository in '..\..\src\cadastros\fabricante\model\repository\impl\Avalicacao.Cadastros.Fabricante.Model.Entity.Impl.FabricanteRepository.pas',
+  Avalicacao.Cadastros.Fabricante.Model.Entity.FabricanteRepository in '..\..\src\cadastros\fabricante\model\repository\Avalicacao.Cadastros.Fabricante.Model.Entity.FabricanteRepository.pas';
 
 {$R *.res}
 
